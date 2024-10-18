38dc8625ba3d5222a7de6da89c0895914b2367db
Thiago Torres <torrestube093@gmail.com>
2024-10-18 18:42:17 -0300

    start

---
+++ Front-End/projects/#005 - landingpage/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Landing Page">
+    <meta name="keywords" content="HTML, CSS, Landing Page">
+    <meta name="author" content="Thiago Torres">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Landing page cena</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="img01">
+                <img src="imagens/logo1.png" alt="">
+            </div><!-- logo left -->
+            <div class="img02">
+                <img src="imagens/logo2.png" alt="">
+            </div><!-- logo right -->
+        </div><!-- container -->
+    </header> <!-- header -->
+</body>
+
+</html>
