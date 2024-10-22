8d2fccbc3f450c54e1e9ca9d049e7771d20bc423
Thiago Torres <torrestube093@gmail.com>
2024-10-22 12:09:11 -0300

    landing page

--- Front-End/projects/#005 - landingpage/index.php
+++ Front-End/projects/#005 - landingpage/index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Landing Page">
-    <meta name="keywords" content="HTML, CSS, Landing Page">
+    <meta name="keywords" content="HTML,CSS,Landing Page">
     <meta name="author" content="Thiago Torres">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Landing page cena</title>
+    <title>Landing page Cena</title>
 </head>
 
 <body>
@@ -20,8 +20,24 @@
             <div class="img02">
                 <img src="imagens/logo2.png" alt="">
             </div><!-- logo right -->
+            <div class="clear"></div>
         </div><!-- container -->
     </header> <!-- header -->
+
+    <div class="container">
+        <div class="banner-form">
+            <div class="banner"></div>
+            <form action="">
+                <h2>PODE ACREDITAR, POR DENTRO É AINDA MAIS BONITO</h2>
+                <label for="name">*Nome completo</label>
+                <input type="text" name="name" id="name">
+                <label for="email">*E-mail</label>
+                <input type="email" name="email" id="email">
+                <label for="number">*Fone</label>
+                <input type="tel" name="number" id="number">
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
